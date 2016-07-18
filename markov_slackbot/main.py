64c16db4272a75a8160a819c820f0e63b0b8f9e7
Stuart Squires <stuartjsquires@gmail.com>
2016-07-18 16:42:53 -0400

    Add docstring for main

--- markov_slackbot/main.py
+++ markov_slackbot/main.py
@@ -6,7 +6,9 @@ from markov_slackbot import MarkovSlackbot
 
 
 def markov_slackbot(config_file):
-    """
+    """API equivalent to using markov_slackbot at the command line.
+
+    :param config_file: User configureation path file.
     """
 
     config = json.loads(open(config_file).read())
