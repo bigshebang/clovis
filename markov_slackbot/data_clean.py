35178526878e1d5bf6b559e913560ae7ad3aec9a
Stuart Squires <stuartjsquires@gmail.com>
2016-07-19 11:46:23 -0400

    Remove data class

--- markov_slackbot/data_clean.py
+++ markov_slackbot/data_clean.py
@@ -1,80 +1,4 @@
-import pandas as pd
-from os import path, listdir
-
-
-class Data(object):
-    def __init__(self):
-        messages_dir = path.join(path.pardir, 'data', 'messages')
-        self.messages_file = open(path.join(messages_dir, 'messages.txt'), 'a')
-        self.messages_file.seek(0)
-        self.messages_file.truncate()
-
-    def clean_data(self, data):
-        """Removes all messages that contain subtypes (ie. not clean messages
-        (comments, file uploads).
-        :param data: A file containing Slack logs.
-        """
-        # regex will clean all reaction from messages
-        messages = data[(data['type'] == 'message') &
-                        ('subtype' not in data.columns)]
-
-        # replace in line sections (ie ``` code ```)
-        messages['text'] = messages['text'].str.replace(r'(```)[^;]*(```)', '')
-        # replace reaction in text
-        messages['text'] = messages['text'].str.replace(r'(:)[^<>]*(:)', '')
-        # replace every <> except for mentions
-        messages['text'] = messages['text'].str.replace(r'(<)[^!@<>]*(>)', '')
-        # replace &gt;
-        messages['text'] = messages['text'].str.replace('&gt;', '')
-        # replace ": "
-        messages['text'] = messages['text'].str.replace(': ', '')
-        # replace " \\' " with " ' "
-        messages['text'] = messages['text'].str.replace("\\'", "'")
-        # replace &amp;
-        # messages['text'] = messages['text'].str.replace('&amp;', '')
-        # encode to string
-        return messages['text'].astype(str)
-
-    def write_to_file(self, messages):
-        """Open file and append messages to text.
-
-        :param messages: Messages to append.
-        """
-
-        messages_file = open(path.join(path.pardir, 'data', 'messages',
-                             'messages.txt'), 'a')
-
-        for message in messages:
-            if(len(message.strip(' ')) > 0):
-                last_char = message.strip(' ')[-1][-1]
-                if (not last_char == '.' and
-                    not last_char == '?' and
-                    not last_char == '!' and
-                    not last_char == '\n' and
-                   len("".join(message.split(' '))) > 0 and
-                   len(message) > 0):
-                    message += '. '
-                messages_file.write(message)
-        messages_file.close
-
-    def get_data(self):
-        folders = listdir(path.join(path.pardir, 'data'))
-        print(folders)
-        for folder in folders:
-            if folder == 'messages':
-                continue
-            files = listdir(path.join(path.pardir, 'data', folder))
-            for file in files:
-                if('json' in file):
-                    file = pd.read_json(path.join(path.pardir, 'data', folder,
-                                        file))
-                    messages = self.clean_data(file)
-                    self.write_to_file(messages)
-
-
-def main():
-    data = Data()
-    data.get_data()
+# -*- coding: utf-8 -*-
 
 
 def parse_slack_json(raw_file_json):
@@ -103,12 +27,15 @@ def remove_subtypes(data):
     messages['text'] = messages['text'].str.replace(r'(```)[^;]*(```)', '')
     # replace reaction in text
     messages['text'] = messages['text'].str.replace(r'(:)[^<>]*(:)', '')
-    # replace every <>
-    messages['text'] = messages['text'].str.replace(r'(<)[^<>]*(>)', '')
+    # replace every <> except for mentions
+    messages['text'] = messages['text'].str.replace(r'(<)[^!@<>]*(>)', '')
     # replace &gt;
     messages['text'] = messages['text'].str.replace('&gt;', '')
-    # replace &amp;
-    messages['text'] = messages['text'].str.replace('&amp;', '')
+    # replace ": "
+    messages['text'] = messages['text'].str.replace(': ', '')
+    # replace " \\' " with " ' "
+    messages['text'] = messages['text'].str.replace("\\'", "'")
+
     # encode to string
     return messages['text'].astype(str)
 
@@ -123,15 +50,15 @@ def add_punctuation(stripped_data):
     punctuated_data = ''
 
     for message in stripped_data:
-        if ((not message.endswith('.') or
-            not message.endswith('?') or
-           not message.endswith('!')) and
-           len(message) > 0):
-            message += '. '
-        punctuated_data += message
+        if(len(message.strip(' ')) > 0):
+            last_char = message.strip(' ')[-1][-1]
+            if (not last_char == '.' and
+                not last_char == '?' and
+                not last_char == '!' and
+                not last_char == '\n' and
+               len("".join(message.split(' '))) > 0 and
+               len(message) > 0):
+                message += '. '
+            punctuated_data += message
 
     return punctuated_data
-
-
-if __name__ == "__main__":
-    main()
