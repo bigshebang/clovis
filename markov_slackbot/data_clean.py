c4bc615e885ed01396a5d29512ee98531cf0cf71
Stuart Squires <stuartjsquires@gmail.com>
2016-07-19 11:37:51 -0400

    Refactor data_clean, implement more commands

--- markov_slackbot/data_clean.py
+++ markov_slackbot/data_clean.py
@@ -17,7 +17,6 @@ class Data(object):
     def clean_data(self, data):
         """Removes all messages that contain subtypes (ie. not clean messages
         (comments, file uploads).
-
         :param data: A file containing Slack logs.
         """
 
@@ -40,7 +39,6 @@ class Data(object):
 
     def write_to_file(self, messages):
         """Open file and append messages to text.
-
         :param messages: Messages to append.
         """
 
@@ -76,6 +74,62 @@ def main():
     """
     data = Data()
     data.get_data()
+
+
+def parse_slack_json(raw_file_json):
+    """Clean raw Slack logs.
+
+    :param raw_file_json: The json to clean.
+    :returns: The cleaned data.
+    """
+
+    stripped_data = remove_subtypes(raw_file_json)
+    clean_data = add_punctuation(stripped_data)
+    return clean_data
+
+
+def remove_subtypes(data):
+    """Removes all messages that contain subtypes (ie. not clean messages
+    (comments, file uploads).
+    :param data: A file containing Slack logs.
+    """
+
+    # regex will clean all reaction from messages
+    messages = data[(data['type'] == 'message') &
+                    ('subtype' not in data.columns)]
+
+    # replace in line sections (ie ``` code ```)
+    messages['text'] = messages['text'].str.replace(r'(```)[^;]*(```)', '')
+    # replace reaction in text
+    messages['text'] = messages['text'].str.replace(r'(:)[^<>]*(:)', '')
+    # replace every <>
+    messages['text'] = messages['text'].str.replace(r'(<)[^<>]*(>)', '')
+    # replace &gt;
+    messages['text'] = messages['text'].str.replace('&gt;', '')
+    # replace &amp;
+    messages['text'] = messages['text'].str.replace('&amp;', '')
+    # encode to string
+    return messages['text'].astype(str)
+
+
+def add_punctuation(stripped_data):
+    """Add periods to messages with no punctuation.
+
+    :param stripped_data: Already stripped down Slack data.
+    :returns punctuated_data: Data with punctuation.
+    """
+
+    punctuated_data = ''
+
+    for message in stripped_data:
+        if ((not message.endswith('.') or
+            not message.endswith('?') or
+           not message.endswith('!')) and
+           len(message) > 0):
+            message += '. '
+        punctuated_data += message
+
+    return punctuated_data
 
 
 if __name__ == "__main__":
--- markov_slackbot/main.py
+++ markov_slackbot/main.py
@@ -1,18 +1,68 @@
 # -*- coding: utf-8 -*-
 
 import json
+import pandas as pd
+from os import path, makedirs, walk
 
 from markov_slackbot import MarkovSlackbot
+from data_clean import parse_slack_json
 
 
 def markov_slackbot(config_file):
     """API equivalent to using markov_slackbot at the command line.
 
-    :param config_file: User configureation path file.
+    :param config_file: User configuration path file.
     """
 
     config = json.loads(open(config_file).read())
-    print(config)
 
     markov_slackbot = MarkovSlackbot(config)
     markov_slackbot.start()
+
+
+def clean_raw_slack_json(raw_filepath, clean_filepath):
+    """Clean raw Slack data from raw_filepath and write it to clean_filepath.
+
+    :param raw_filepath: Raw Slack data file
+    :param clean_filepath: Clean Slack data file
+    """
+
+    raw_file_json = pd.read_json(raw_filepath)
+    clean_data = parse_slack_json(raw_file_json)
+
+    clean_data_file = open(clean_filepath, 'a')
+    clean_data_file.seek(0)
+    clean_data_file.truncate()
+
+    clean_data_file.write(clean_data)
+
+
+def clean_raw_slack_dir(raw_dir, clean_dir):
+    """Clean Slack log directory, outputting a new directory with preserved
+    structure.
+
+    :param raw_dir: The directory of raw slack logs.
+    :param clean_dir: The directory to write cleaned logs to.
+    """
+
+    if not path.exists(clean_dir):
+        makedirs(clean_dir)
+
+    # Do a recursive walk over the raw directory
+    for root, subdirs, files in walk(raw_dir):
+        # Create subdirectories in the clean directory
+        for subdir in subdirs:
+            clean_subdir = path.join(clean_dir, subdir)
+            if not path.exists(clean_subdir):
+                makedirs(clean_subdir)
+
+        for raw_filename in files:
+            raw_filepath = path.join(root, raw_filename)
+
+            # Creating the new root by replacing the first part of the path
+            clean_root = root.replace(raw_dir, clean_dir, 1)
+
+            clean_filename = raw_filename.replace('.json', '.txt')
+            clean_filepath = path.join(clean_root, clean_filename)
+
+            clean_raw_slack_json(raw_filepath, clean_filepath)
